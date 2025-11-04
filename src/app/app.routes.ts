9faad4f4fc87ac96b1e18fedb1c29c91b80cd6d1
Richard Burman <richard.burman@harperjames.co.uk>
2025-11-04 11:57:31 +0000

    add user route to navigate dynamically to user tasks component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
     { path: '', // <your-domain>
         component: NoTaskComponent, 
     },
-    { path: 'tasks', // <your-domain>/tasks
-        component: TasksComponent, 
+    { path: 'users/:userId', // <your-domain>/users/:userId
+        component: UserTasksComponent, 
     },
 ];
