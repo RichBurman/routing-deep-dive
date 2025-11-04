aa59a75df5f7b66a8344e19ff32736ab7aed8817
Richard Burman <richard.burman@harperjames.co.uk>
2025-11-04 15:46:21 +0000

    add users.routes.ts for task routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './tasks/tasks.component';
+import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { Component } from '@angular/core';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
@@ -12,21 +12,7 @@ export const routes: Routes = [
     },
     { path: 'users/:userId', // <your-domain>/users/:userId
         component: UserTasksComponent, 
-        children: [
-            {
-                path: '',
-                redirectTo: 'tasks',
-                pathMatch: 'prefix'
-            },
-            {
-            path: 'tasks', // <your-domain/>users/userid/tasks
-            component: TasksComponent
-            },
-            {
-                path: 'tasks/new',
-                component: NewTaskComponent
-            }
-        ]
+        children: userRoutes
     },
     {
         path: '**',
