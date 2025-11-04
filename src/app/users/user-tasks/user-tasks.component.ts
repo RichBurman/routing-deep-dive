338e41704a27a8a953c3495ff07d1c34583da199
Richard Burman <richard.burman@harperjames.co.uk>
2025-11-04 16:30:05 +0000

    refactor tasks and user-tasks components to enhance routing and logging functionality by using queryParams

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -23,6 +23,7 @@ export class UserTasksComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.activatedRoute);
+    console.log(this.activatedRoute.snapshot.paramMap.get('userId'));
     const subscription = this.activatedRoute.paramMap.subscribe({
       next: (paramMap) => {
         this.userName =
