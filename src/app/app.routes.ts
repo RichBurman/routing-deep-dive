a956869ee84b03fca2502a836ef49714b64f0ea1
Richard Burman <richard.burman@harperjames.co.uk>
2025-11-06 11:26:04 +0000

    refactor routes to enhance user task handling and improve data resolution by add canMatch and creating dummyCanMatch function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,45 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { Component } from '@angular/core';
+import {
+  resolveTitle,
+  resolveUserName,
+  UserTasksComponent,
+} from './users/user-tasks/user-tasks.component';
+import { Component, inject } from '@angular/core';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));;
+};
+
 export const routes: Routes = [
-    { path: '', // <your-domain>
-        component: NoTaskComponent, 
-        title: 'No Task selected'
+  {
+    path: '', // <your-domain>
+    component: NoTaskComponent,
+    title: 'No Task selected',
+  },
+  {
+    path: 'users/:userId', // <your-domain>/users/:userId
+    component: UserTasksComponent,
+    children: userRoutes,
+    canMatch: [dummyCanMatch],
+    data: {
+      message: 'Hello',
     },
-    { path: 'users/:userId', // <your-domain>/users/:userId
-        component: UserTasksComponent, 
-        children: userRoutes,
-        data: {
-            message: 'Hello'
-        },
-        resolve: {
-            userName: resolveUserName
-        },
-        title: resolveTitle
+    resolve: {
+      userName: resolveUserName,
     },
-    {
-        path: '**',
-        component: NotFoundComponent
-    }
+    title: resolveTitle,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
